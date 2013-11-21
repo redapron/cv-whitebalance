96ab9e9348071b120a40123ef9c37c8f0262f750
bui <bungbui@gmail.com>
2013-11-21 18:25:44 +0700

    รอส่ง
    เทียบกับเขียว

--- Lesson1/_main.cpp
+++ Lesson1/_main.cpp
@@ -8,15 +8,59 @@
 #include <opencv2\imgproc.hpp>
 
 using namespace std;
+using namespace cv;
+
+int hw=1, sw=1, vw=1;				// input color
+int static hc=54, sc=115, vc=124;	// real color
+
+void onMouse(int event, int x, int y, int flags, void* param)
+{
+	if (event == EVENT_LBUTTONDOWN) {
+		Mat frame;
+		frame = *((Mat*)param);
+		
+		cvtColor(frame, frame, CV_BGR2HSV);
+		hw = frame.at<Vec3b>(y,x)[0];
+		sw = frame.at<Vec3b>(y,x)[1];
+		vw = frame.at<Vec3b>(y,x)[2];
+
+		Mat picker(100, 100, CV_8UC3, frame.at<Vec3b>(y,x));
+		cvtColor(picker, picker, CV_HSV2BGR);
+		imshow("picker", picker);
+
+		cout << frame.at<Vec3b>(y,x) << endl;
+	}
+}
+void _onMouse(int event, int x, int y, int flags, void* param)
+{
+	if (event == EVENT_LBUTTONDOWN) {
+		Mat frame;
+		frame = *((Mat*)param);
+		cvtColor(frame, frame, CV_BGR2HSV);
+		cout << frame.at<Vec3b>(y,x) << endl;
+	}
+}
+
+Mat canvas(480, 640, CV_8UC3);
+Mat _image;
 
 cv::Mat image;
 cv::Mat hsvImg;
 
 cv::VideoCapture cam;
 
-int thres[12][6] = { {0, 20, 50, 255, 50, 255}, {0, 0, 50, 255, 50, 255}, {0, 0, 50, 255, 50, 255},
-					{0, 20, 50, 255, 50, 255}, {0, 0, 50, 255, 50, 255}, {0, 0, 50, 255, 50, 255},
-					{0, 20, 50, 255, 50, 255}, {0, 0, 50, 255, 50, 255}, {0, 0, 50, 255, 50, 255}
+int thres[12][6] = {	{144, 146, 50, 255, 50, 255}, // 1
+						{146, 148, 50, 255, 50, 255},  
+						{119, 121, 50, 255, 50, 255},
+						{97, 99, 50, 255, 50, 255}, 
+						{100, 102, 50, 255, 50, 255}, // 5
+						{92, 94, 50, 255, 50, 255},
+						{84, 86, 50, 255, 50, 255}, 
+						{52, 54, 50, 255, 50, 255}, 
+						{23, 25, 50, 255, 50, 255},
+						{17, 19, 50, 255, 50, 255},	  // 10
+						{6, 8, 50, 255, 50, 255}, 
+						{164, 166, 50, 255, 50, 255}
 				   };
 int currColor = 0;
 
@@ -37,10 +81,43 @@ int main()
 		//cout << "time : " << clock() - startTime << endl;
 		//startTime = clock();
 
-		//cam >> image;
-		image = cv::imread("C:\\Users\\bui\\Desktop\\_000-sensor\\Hsv_Value_Chart.png");
-		cv::cvtColor(image, hsvImg, CV_BGR2HSV);
+		cam >> image;
+		//image = cv::imread("C:\\Users\\bui\\Desktop\\_000-sensor\\cv-whitebalance\\data\\image1384353593.bmp");
+		//cv::cvtColor(image, hsvImg, CV_BGR2HSV);
 		cv::imshow("image", image);
+//
+		cv::cvtColor(image, _image, CV_BGR2HSV);
+
+		for (int i=0; i<_image.rows; i++)
+			for (int k=0; k<_image.cols; k++) {
+			
+				if (hw==0) {
+					canvas.at<Vec3b>(i,k)[0] =  _image.at<Vec3b>(i,k)[0];
+				} else {
+					canvas.at<Vec3b>(i,k)[0] = (_image.at<Vec3b>(i,k)[0] * hc / hw) > 180  
+												? 180 : _image.at<Vec3b>(i,k)[0] * hc / hw;
+				}
+				if (sw==0) {
+					canvas.at<Vec3b>(i,k)[1] = _image.at<Vec3b>(i,k)[1];
+				} else {
+					canvas.at<Vec3b>(i,k)[1] = (_image.at<Vec3b>(i,k)[1] * sc / sw) > 255  
+												? 255 : _image.at<Vec3b>(i,k)[1] * sc / sw;
+				}
+				if (vw==0) {
+					canvas.at<Vec3b>(i,k)[2] = _image.at<Vec3b>(i,k)[2];
+				}
+				else {
+					canvas.at<Vec3b>(i,k)[2] = (_image.at<Vec3b>(i,k)[2] * vc / vw) > 255  
+												? 255 : _image.at<Vec3b>(i,k)[2] * vc / vw;
+				}
+			
+			}
+
+
+		hsvImg=canvas;
+
+		setMouseCallback("image", onMouse, &image);
+//
 
 		for(int k = 0; k < 12; k++)
 		{
@@ -93,6 +170,7 @@ int main()
 			}
 		}
 		cv::imshow("hsvImg", hsvImg);
+		setMouseCallback("hsvImg", _onMouse, &hsvImg);
 
 		char c = cv::waitKey(10);
 
@@ -102,6 +180,15 @@ int main()
 		if( c == '1' )	currColor = 0;
 		if( c == '2' )	currColor = 1;
 		if( c == '3' )	currColor = 2;
+		if( c == '4' )	currColor = 3;
+		if( c == '5' )	currColor = 4;
+		if( c == '6' )	currColor = 5;
+		if( c == '7' )	currColor = 6;
+		if( c == '8' )	currColor = 7;
+		if( c == '9' )	currColor = 8;
+		if( c == '0' )	currColor = 9;
+		if( c == '-' )	currColor = 10;
+		if( c == '=' )	currColor = 11;
 
 		switch(c)
 		{

11d5aca14ae787fae28fa1aab34c174235be537c
bui <bungbui@gmail.com>
2013-11-23 04:43:32 +0700

    solvepnp หาจุดบน cube

--- Lesson1/_main.cpp
+++ Lesson1/_main.cpp
@@ -6,35 +6,33 @@
 #include <opencv\cv.h>
 #include <opencv2\highgui.hpp>
 #include <opencv2\imgproc.hpp>
+#include <opencv2\calib3d\calib3d.hpp>
 
 using namespace std;
 using namespace cv;
 
-int hw=1, sw=1, vw=1;				// input color
-int static hc=54, sc=115, vc=124;	// real color
-
+VideoCapture cam;
 Mat canvas(480, 640, CV_8UC3);
 Mat _image;
 Mat image;
 Mat hsvImg;
 
-VideoCapture cam;
-
-int thres[12][6] = {	{144, 146, 50, 255, 50, 255}, // 1
+int hw=1, sw=1, vw=1;				// input color
+int static hc=54, sc=115, vc=124;	// real color
+int thres[12][6] = {	{157, 158, 50, 255, 50, 255}, // 0
 						{146, 148, 50, 255, 50, 255},  
 						{135, 137, 50, 255, 50, 255},
 						{97, 99, 50, 255, 50, 255}, 
-						{100, 102, 50, 255, 50, 255}, // 5
-						{92, 94, 50, 255, 50, 255},
+						{100, 102, 50, 255, 50, 255}, // 4
+						{92, 94, 50, 255, 50, 255},	  // 5
 						{84, 86, 50, 255, 50, 255}, 
 						{52, 54, 50, 255, 50, 255}, 
 						{23, 25, 50, 255, 50, 255},
-						{17, 19, 50, 255, 50, 255},	  // 10
+						{17, 19, 50, 255, 50, 255},	  // 9
 						{6, 8, 50, 255, 50, 255}, 
 						{0, 3, 50, 255, 50, 255}
 				   };
 int currColor = 0;
-
 int center[12][2];
 int pixelCnt[12];
 
@@ -43,8 +41,8 @@ void onMouse(int event, int x, int y, int flags, void* param)
 {
 	Mat frame;
 	frame = *((Mat*)param);
+	cvtColor(frame, frame, CV_BGR2HSV);
 	if (event == EVENT_LBUTTONDBLCLK) {
-		cvtColor(frame, frame, CV_BGR2HSV);
 		hw = frame.at<Vec3b>(y,x)[0];
 		sw = frame.at<Vec3b>(y,x)[1];
 		vw = frame.at<Vec3b>(y,x)[2];
@@ -54,8 +52,8 @@ void onMouse(int event, int x, int y, int flags, void* param)
 		Mat picker(100, 100, CV_8UC3, frame.at<Vec3b>(y,x));
 		cvtColor(picker, picker, CV_HSV2BGR);
 		imshow("picker", picker);
-
-		cout << frame.at<Vec3b>(y,x) << endl;
+		cout << frame.at<Vec3b>(y,x) << endl;	// color in hsv
+		//cout << x << ", " << y << endl;			// coordinate x,y
 	}
 }
 
@@ -212,17 +210,98 @@ void checkForKeyboardInput(char c){
 	}
 }
 
+// for drawing a cube
+Mat intrinsics, distortion;
+Mat rt, cubeCoor[4];
+vector<Point2f> imagePoint;
+vector<Point3f> worldPoint;
+
+vector<double> rv(3), tv(3);
+Mat rotateVector(rv), translateVector(tv);
+/**
+	
+*/
+void drawCube_init(){
+	FileStorage fs("C:\\Users\\bui\\Desktop\\camera.yml", FileStorage::READ);
+	fs["camera_matrix"] >> intrinsics;
+	fs["distortion_coefficients"] >> distortion;
+
+	worldPoint.push_back(Point3f(0.0f, 0.0f, 0.0f));
+	worldPoint.push_back(Point3f(0.0f, 1.0f, 0.0f));
+	worldPoint.push_back(Point3f(1.0f, 0.0f, 0.0f));
+	worldPoint.push_back(Point3f(1.0f, 1.0f, 0.0f));
+
+	cubeCoor[0] = (Mat_<double>(4,1) << 0, 0, 1, 1); 
+	cubeCoor[1] = (Mat_<double>(4,1) << 0, 1, 1, 1);
+	cubeCoor[2] = (Mat_<double>(4,1) << 1, 0, 1, 1);
+	cubeCoor[3] = (Mat_<double>(4,1) << 1, 1, 1, 1);
+}
+
+Mat rotateMatrix;
+double *r, *t;
+
+void drawCube_solvePnP(){
+	imagePoint.clear();
+	imagePoint.push_back(Point2f(center[5][0], center[5][1]));
+	imagePoint.push_back(Point2f(center[7][0], center[7][1]));
+	imagePoint.push_back(Point2f(center[0][0], center[0][1]));
+	imagePoint.push_back(Point2f(center[4][0], center[4][1]));
+
+	/*
+	imagePoint.push_back(Point2f(166, 335));
+	imagePoint.push_back(Point2f(244, 275));
+	imagePoint.push_back(Point2f(264, 388));
+	imagePoint.push_back(Point2f(333, 325));
+	*/
+	solvePnP(Mat(worldPoint), Mat(imagePoint), intrinsics, distortion, rotateVector, translateVector, false, CV_ITERATIVE);
+}
+
+void drawCube_draw(){
+	Rodrigues(rotateVector,rotateMatrix);
+	r = rotateMatrix.ptr<double>();
+	t = translateVector.ptr<double>();
+	rt = (Mat_<double>(3,4) << r[0], r[1], r[2], t[0], r[3], r[4], r[5], t[1], r[6], r[7], r[8], t[2]);
+
+	for (int k=0; k<4; k++){
+
+		Mat ans = intrinsics * rt * cubeCoor[k];
+		double* ans_array = ans.ptr<double>();
+		ans_array[0] = ans_array[0]/ans_array[2];
+		ans_array[1] = ans_array[1]/ans_array[2];
+		ans_array[2] = ans_array[2]/ans_array[2];
+		Mat newAns = (Mat_<double>(3,1) << ans_array[0], ans_array[1], ans_array[2]);
+
+		Point newPoint = Point(ans_array[0],ans_array[1]);
+		imagePoint.push_back(newPoint);
+
+		/*
+		// debug
+		cout << "intrinsics" << endl << intrinsics << endl << endl;
+		cout << "rotate" << endl << rotateMatrix << endl << endl;
+		cout << "translate" << endl << translateVector << endl << endl;
+		cout << "rt" << endl << rt << endl << endl;
+		cout << "ans" << endl << ans << endl << endl;
+		cout << "newAns" << endl << newAns << endl << endl;
+		*/
+	}
+
+	for (int k=0; k<8; k++){
+		circle(image, imagePoint[k], 5, Scalar(0,222,0),-1);
+		line(image, imagePoint[k], imagePoint[(k+1)%8], Scalar(0,222,0),2);
+	}
+}
+
 int main()
 {
 	cam.open(0);
 
+	drawCube_init();
+
 	while(true)
 	{
-		setMouseCallback("image", onMouse, &image);
-
 		//cam >> image;
 		image = imread("C:\\Users\\bui\\Desktop\\_000-sensor\\cv-whitebalance\\data\\image1384353593.bmp");
-		imshow("image", image);
+		setMouseCallback("image", onMouse, &image);
 		
 		// cvt image to hsvImg
 		cvtColor(image, hsvImg, CV_BGR2HSV);
@@ -233,14 +312,18 @@ int main()
 		countingPixelPaintItAndFindItCenter();
 		cvtColor(hsvImg, hsvImg, CV_HSV2BGR);
 		printPixelNumberAtItCenter();
+
+		drawCube_solvePnP();
+		drawCube_draw();
+
 		imshow("hsvImg", hsvImg);
+		imshow("image", image);
 
 		char c = waitKey(10);
 		if( c == 27 )
 			break;
 		//checkForKeyboardInput(c);
 	}
-
 	cam.release();
 
 	return 0;
